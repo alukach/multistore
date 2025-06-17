2476c1b4e108ead74a1354766e35c6c53a645de0
Anthony Lukach <anthonylukach@gmail.com>
2025-06-16 21:15:15 -0700

    Example of simple CPU failure on CF Workers

--- examples/cf-workers-api/src/lib.rs
+++ examples/cf-workers-api/src/lib.rs
@@ -1,41 +1,63 @@
 use console_error_panic_hook;
-use multistore::credentials::static_auth::StaticCredentialsRegistry;
-use multistore::data_source::static_db::StaticDataSourceRegistry;
-use multistore::s3::S3Interface;
-use s3s::{service::S3ServiceBuilder, S3Error};
-use worker::{event, Context, Env, HttpRequest};
+use object_store::{aws::AmazonS3Builder, path::Path};
+use worker::{event, Context, Env, HttpRequest, Response, Result};
 
 #[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> s3s::S3Result<http::Response<s3s::Body>> {
+async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> Result<Response> {
     // Initialize panic hook for better error messages
     console_error_panic_hook::set_once();
 
-    let config: serde_yaml::Value =
-        serde_yaml::from_str(include_str!("../../../database.yaml")).unwrap();
+    let store = AmazonS3Builder::from_env()
+        .with_bucket_name("overturemaps-us-west-2")
+        .build()
+        .map_err(|e| worker::Error::RustError(e.to_string()))?;
 
-    let creds_registry = StaticCredentialsRegistry::from_serde(config.clone());
-    let data_source_registry = StaticDataSourceRegistry::from_serde(config.clone());
-    let s3_backend = S3Interface::new(data_source_registry);
+    let object = store
+        .get(&Path::from("/release/2025-05-21.0/theme=buildings/type=building/part-00006-0df994ca-3323-4d7c-a374-68c653f78289-c000.zstd.parquet"))
+        .await
+        .map_err(|e| worker::Error::RustError(e.to_string()))?;
 
-    let service = {
-        let mut builder = S3ServiceBuilder::new(s3_backend);
-        builder.set_auth(creds_registry);
-        builder.build()
-    };
+    let mut headers = worker::Headers::new();
+    headers.append("Content-Type", "application/octet-stream")?;
+    headers.append("Transfer-Encoding", "chunked")?;
 
-    // Convert the request and handle it
-    let req = req.map(|body| s3s::Body::http_body(body));
-    service.call(req).await
+    let stream = object
+        .into_stream()
+        .map_err(|e| worker::Error::RustError(e.to_string()));
+    Response::from_stream(stream).map(|resp| resp.with_headers(headers))
 }
 
-struct ApiError(S3Error);
+// #[event(fetch)]
+// async fn fetch(
+//     req: HttpRequest,
+//     _env: Env,
+//     _ctx: Context,
+// ) -> s3s::S3Result<http::Response<s3s::Body>> {
+//     // Initialize panic hook for better error messages
+//     console_error_panic_hook::set_once();
 
-impl From<ApiError> for worker::Error {
-    fn from(e: ApiError) -> Self {
-        worker::Error::RustError(e.0.to_string())
-    }
-}
+//     let config: serde_yaml::Value =
+//         serde_yaml::from_str(include_str!("../../../database.yaml")).unwrap();
+
+//     let creds_registry = StaticCredentialsRegistry::from_serde(config.clone());
+//     let data_source_registry = StaticDataSourceRegistry::from_serde(config.clone());
+//     let s3_backend = S3Interface::new(data_source_registry);
+
+//     let service = {
+//         let mut builder = S3ServiceBuilder::new(s3_backend);
+//         builder.set_auth(creds_registry);
+//         builder.build()
+//     };
+
+//     // Convert the request and handle it
+//     let req = req.map(|body| s3s::Body::http_body(body));
+//     service.call(req).await
+// }
+
+// struct ApiError(S3Error);
+
+// impl From<ApiError> for worker::Error {
+//     fn from(e: ApiError) -> Self {
+//         worker::Error::RustError(e.0.to_string())
+//     }
+// }

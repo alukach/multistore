9797082bcef0c8936ba9de58b1a2ec44e4b11e50
Anthony Lukach <anthonylukach@gmail.com>
2025-11-30 14:44:47 -0800

    chore: improve logging in hyper example

--- examples/hyper-api/src/utils.rs
+++ examples/hyper-api/src/utils.rs
@@ -3,7 +3,12 @@ use std::io::IsTerminal;
 pub fn setup_tracing() {
     use tracing_subscriber::EnvFilter;
 
-    let env_filter = EnvFilter::from_default_env();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        EnvFilter::new("info")
+            .add_directive("hyper_api=info".parse().unwrap())
+            .add_directive("multistore=warn".parse().unwrap())
+    });
+
     let enable_color = std::io::stdout().is_terminal();
 
     tracing_subscriber::fmt()
